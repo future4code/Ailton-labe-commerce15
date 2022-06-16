10f6d6c4038fb6432466195997711cc24afe8eac
thailaesteves <104744254+thailaesteves@users.noreply.github.com>
2022-06-16 01:03:39 -0300

    arrumando os botoes do carrinho

--- src/Components/Produtos.js
+++ src/Components/Produtos.js
@@ -1,40 +1,61 @@
-import React from "react";
-import styled from "styled-components";
+// import React from "react";
+// import styled from "styled-components";
 
-export const BoxPrincipal = styled.div`
-  display: grid;
-  grid-template-rows: 3fr 1fr;
-  grid-template-columns: 1fr;
-  border: 1px solid black;
-  align-items: start;
-  justify-items: center;
-  margin: 20px;
-  height: 35vh;
-  width: 80%;
-`
+// const BoxPrincipal = styled.div`
+//   display: grid;
+//   grid-template-rows: 3fr 1fr;
+//   grid-template-columns: 1fr;
+//   border: 1px solid black;
+//   align-items: start;
+//   justify-items: center;
+//   margin: 20px;
+//   height: 35vh;
+//   width: 80%;
+// `
 
-export const ProdutoImg = styled.img`
-height: 100%;
-width: 100%;
-`
-export const BotaoCompra = styled.button`
-margin-bottom: 5px;
-`
+// const ProdutoImg = styled.img`
+// height: 100%;
+// width: 100%;
+// `
+// const BotaoCompra = styled.button`
+// margin-bottom: 5px;
+// `
 
-const Produtos = (props) => {
-  function AdicionaCarrinho() {
-    alert("Produto Adicionado ao Carrinho");
-    
-  }
-  console.log(props.variavel);
-  return (
-    <BoxPrincipal >
-      <ProdutoImg src={props.imagem} alt={props.textoAlt} />
-      <h4>{props.titulo}</h4>
-      <h4>{props.preco}</h4>
-      <BotaoCompra onClick={AdicionaCarrinho}>Adicionar ao Carrinho</BotaoCompra>
-    </BoxPrincipal>
-  );
-};
+// class Produtos extends React.Component {
+//   state = {
+//     valorCarrinho: 0
+//   }
 
-export default Produtos;
+//   adicionaCarrinho = () => {
+//     const carrinhoSoma = this.state.valorCarrinho + 1;
+//     alert("Produto Adicionado ao Carrinho");
+//     console.log(this.state.valorCarrinho);
+
+//     this.setState({
+//       valorCarrinho: carrinhoSoma
+
+//     });
+//   }
+
+//   // removeCarrinho = () => {
+//   //   const carrinhoSubtrair = this.state.valorCarrinho -1;
+//   //   alert("Produto Removido ao Carrinho")
+
+//   //   this.setState({
+//   //     valorCarrinho: carrinhoSubtrair
+//   //   });
+//   // }
+  
+//   render() {
+//     return (
+//       <BoxPrincipal >
+//         <ProdutoImg src={this.props.imagem} alt={this.props.textoAlt} />
+//         <h4>{this.props.titulo}</h4>
+//         <h4>{this.props.preco}</h4>
+//         <BotaoCompra onClick={this.adicionaCarrinho}>Adicionar ao Carrinho</BotaoCompra>
+//       </BoxPrincipal>
+//     );
+//   };
+// };
+
+// export default Produtos;
